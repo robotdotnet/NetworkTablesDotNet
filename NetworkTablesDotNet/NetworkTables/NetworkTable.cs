4b47d9b3dd0665a8ccb92769aca756fd8eec3d2d
ThadHouse <thad@athometech.com>
2015-05-20 21:00:10 -0700

    Initial Commit. Just going to be used to get the interfaces.

---
+++ NetworkTablesDotNet/NetworkTables/NetworkTable.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NetworkTablesDotNet.Tables;
+using NetworkTablesDotNet.NetworkTables2.Thread;
+
+namespace NetworkTablesDotNet.NetworkTables
+{
+    public class NetworkTable : ITable, IRemote
+    {
+        private static NTThreadManager threadManager = new DefaultThreadManager();
+
+        public static readonly char PATH_SEPARATOR = '/';
+        public static readonly int DEFAULT_PORT = 1735;
+
+        public bool ContainsKey(string key)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool ContainsSubTable(string key)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ITable GetSubTable(string key)
+        {
+            throw new NotImplementedException();
+        }
+
+        public object GetValue(string key)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void PutValue(string key, object value)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RetrieveValue(string key, object externalValue)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void PutNumber(string key, double value)
+        {
+            throw new NotImplementedException();
+        }
+
+        public double GetNumber(string key)
+        {
+            throw new NotImplementedException();
+        }
+
+        public double GetNumber(string key, double defaultValue)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void PutString(string key, string value)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string GetString(string key)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string GetString(string key, string defaultValue)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void PutBoolean(string key, bool value)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool GetBoolean(string key)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool GetBoolean(string key, bool defaultValue)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void AddTableListener(ITableListener listener)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void AddTableListener(ITableListener listener, bool immediateNotify)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void AddTableListener(string key, ITableListener listener, bool immediateNotify)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void AddSubTableListener(ITableListener listener)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RemoveTableListener(ITableListener listener)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void AddConnectionListener(IRemoteConnectionListener listener, bool immediateNotify)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RemoveConnectionListener(IRemoteConnectionListener listener)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool IsConnected()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool ISServer()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ NetworkTablesDotNet/Tables/ITableListener.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace NetworkTablesDotNet.Tables
+{
+    public interface ITableListener
+    {
+        void ValueChanged(ITable source, string key, object value, bool isNew);
+    }
+}
