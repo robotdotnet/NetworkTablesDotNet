2287c7505db5be64c3195928435d5e0b18faf79c
ThadHouse <thad@athometech.com>
2015-06-16 19:29:17 -0700

    Fixed Assembly

--- NetworkTables/Properties/AssemblyInfo.cs
+++ NetworkTables/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("FRC NetworkTables")]
+[assembly: AssemblyTitle("NetworkTables")]
 [assembly: AssemblyDescription("A native DotNet version of the FRC NetworkTables library")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("RobotDotNet")]
-[assembly: AssemblyProduct("FRC NetworkTables")]
+[assembly: AssemblyProduct("NetworkTables")]
 [assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
