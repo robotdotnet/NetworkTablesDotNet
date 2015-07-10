b3986b9c83667a74bea711a017c0db5e3279cb80
ThadHouse <thad@athometech.com>
2015-07-09 23:35:51 -0700

    More Unit Tests

---
+++ NetworkTables.Test/NetworkTables2/Connection/BadMessageExceptionTest.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NetworkTables.NetworkTables2.Connection;
+
+namespace NetworkTables.Test.NetworkTables2.Connection
+{
+    [TestClass]
+    public class BadMessageExceptionTest
+    {
+        [TestMethod]
+        public void ThrowExceptionTest()
+        {
+            try
+            {
+                throw new BadMessageException("Got some bad message");
+            }
+            catch (BadMessageException e)
+            {
+                Assert.AreEqual("Got some bad message", e.Message);
+            }
+        }
+    }
+}
---
+++ NetworkTables.Test/NetworkTables2/Connection/ConnectionMonitorThreadTest.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NetworkTables.NetworkTables2.Connection;
+using Telerik.JustMock;
+
+namespace NetworkTables.Test.NetworkTables2.Connection
+{
+    [TestClass]
+    public class ConnectionMonitorThreadTest
+    {
+        private static ConnectionMonitorThread thread;
+
+        private static ConnectionAdapter adapter;
+        private static NetworkTableConnection connection;
+
+        [ClassInitialize]
+        public static void Init(TestContext ctx)
+        {
+            adapter = Mock.Create<ConnectionAdapter>();
+            connection = Mock.Create<NetworkTableConnection>();
+            thread = new ConnectionMonitorThread(adapter, connection);
+        }
+        //These run a thread, and since it blocks, its not running correctly.
+        /*
+        [TestMethod]
+        public static void TestSimpleRead()
+        {
+            thread.Run();
+            
+            Mock.Assert(() => connection.Read(adapter), Occurs.Once()); 
+
+            
+        }
+        */
+    }
+}
