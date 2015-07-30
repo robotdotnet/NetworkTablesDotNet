b995b3710b32574b09b5c49e03df12fc177091f4
ThadHouse <thad@athometech.com>
2015-07-29 18:38:04 -0700

    More UnitTesting

---
+++ NetworkTables.Test/NetworkTables2/Util/StackTest.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NetworkTables.NetworkTables2.Util;
+using Telerik.JustMock.AutoMock.Ninject;
+
+namespace NetworkTables.Test.NetworkTables2.Util
+{
+    [TestClass]
+    public class StackTest
+    {
+        [TestMethod]
+        public void PushTest()
+        {
+            int obj1 = 0;
+            Stack stack = new Stack();
+            Assert.IsTrue(stack.IsEmpty());
+            stack.Push(obj1);
+            Assert.IsFalse(stack.IsEmpty());
+        }
+
+        [TestMethod]
+        public void PopTest()
+        {
+            int obj1 = 0;
+            object obj2;
+            Stack stack = new Stack();
+            Assert.IsTrue(stack.IsEmpty());
+            obj2 = stack.Pop();
+            Assert.IsNull(obj2);
+
+            stack.Push(obj1);
+            obj2 = stack.Pop();
+            Assert.AreEqual(obj1, obj2);
+            Assert.IsTrue(stack.IsEmpty());
+        }
+
+        [TestMethod]
+        public void OrderingTest()
+        {
+            int obj1 = 42;
+            int obj2 = 43;
+            int obj3 = 44;
+            object obj4;
+            object obj5;
+            object obj6;
+            object obj7;
+            Stack stack = new Stack();
+            Assert.IsTrue(stack.IsEmpty());
+            stack.Push(obj1);
+            Assert.IsTrue(stack.HasSize(1));
+            stack.Push(obj2);
+            stack.Push(obj3);
+            Assert.IsTrue(stack.HasSize(3));
+            obj4 = stack.Pop();
+            Assert.IsTrue(stack.HasSize(2));
+            obj5 = stack.Pop();
+            obj6 = stack.Pop();
+            Assert.AreEqual(obj1, obj6);
+            Assert.AreEqual(obj2, obj5);
+            Assert.AreEqual(obj3, obj4);
+
+            stack.Push(obj1);
+            stack.Push(obj2);
+            stack.Push(obj3);
+            stack.Pop();
+            stack.Push(obj1);
+            obj7 = stack.Pop();
+            Assert.AreEqual(obj1, obj7);
+        }
+    }
+
+    public static class StackExtensions
+    {
+        public static bool HasSize(this Stack stack, int size)
+        {
+            return stack.Size() == size;
+        }
+    }
+}
